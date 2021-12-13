98a762d550df7a0b7e73a68e531a5bfe3f85b259
Keita <askeita2017@gmail.com>
2021-12-13 15:54:47 +0300

    Conversion Function from one currency to another currency

--- currencyConverter/currencyConvrter.py
+++ currencyConverter/currencyConvrter.py
@@ -13,3 +13,13 @@ class Currency_convertor:
         # Extracting only the rates from the json data
         self.rates = data["rates"]
 
+        # function to do a simple cross multiplication between
+    # the amount and the conversion rates
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        if from_currency != 'EUR' :
+            amount = amount / self.rates[from_currency]
+
+        # limiting the precision to 2 decimal places
+        amount = round(amount * self.rates[to_currency], 2)
+        print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
